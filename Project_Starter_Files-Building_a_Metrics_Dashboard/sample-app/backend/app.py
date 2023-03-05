136425b3bbfe316d62d2bb746ba9a90d8c98bef5
HoanTV <hoantv@example.com>
2023-03-05 14:56:22 +0700

    #GET_PASSES_THIS_REPO_UDACITY_PLEASE

---
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/sample-app/backend/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+from flask_prometheus_metrics import register_metrics
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
+
+app = Flask(__name__)
+config = Config(
+    config={
+        'sampler':
+        {'type': 'const',
+         'param': 1},
+                        'logging': True,
+                        'reporter_batch_size': 1,}, 
+                        service_name="service")
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
+
+@app.route("/")
+def index():
+  return "Test"
+
+@app.route("/system-error")
+def system_error():
+  1/0
+  return "Test"
+
+# provide app's version and deploy environment/config name to set a gauge metric
+register_metrics(app, app_version="v0.1.2", app_config="staging")
+
+# Plug metrics WSGI app to your main app with dispatcher
+dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+
+run_simple(hostname="0.0.0.0", port=5000, application=dispatcher)
